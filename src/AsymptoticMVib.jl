2dd5ed5b3f4d90f82ea8859cf3415c561320c2d0
RayleighLord <javier.gonzalez.monge@gmail.com>
2025-02-16 21:52:47 +0100

    Repository creation and upload

--- src/AsymptoticMVib.jl
+++ src/AsymptoticMVib.jl
@@ -1,5 +1,23 @@
 module AsymptoticMVib
 
-# Write your package code here.
+using DispatchDoctor: @stable
+
+@stable default_mode="error" begin # Change to disable in production
+    """
+        add(x, y)
+
+    Add two numbers together.
+
+    # Example of usage
+
+    ```julia
+    julia> add(1, 2)
+    3
+    ```
+    """
+    add(x, y) = x + y
+end
+
+export add
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,15 @@
 using AsymptoticMVib
-using Test
-using Aqua
+using TestItems: @testitem
+using TestItemRunner: @run_package_tests
 
-@testset "AsymptoticMVib.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(AsymptoticMVib; ambiguities = false,)
-    end
-    # Write your tests here.
+@run_package_tests
+
+@testitem "Functions" begin
+    @test add(1, 2) == 3
+    @test add(1.0, 2.0) == 3.0
+end
+
+@testitem "Code quality (Aqua.jl)" begin
+    using Aqua
+    Aqua.test_all(AsymptoticMVib; ambiguities = false, deps_compat = (check_extras = false))
 end
